888d01c0d0189d28ce960a7eb30cd9cace3fe824
Feiyu-Yao <frankyao.ece@gmail.com>
2020-12-12 08:47:45 +0800

    with priority

--- rlcode_python/DQN-with-priority-using-eager.py
+++ rlcode_python/DQN-with-priority-using-eager.py
@@ -1,6 +1,7 @@
 import numpy as np
 import gym
 import random
+import matplotlib
 from collections import deque
 from keras.models import Sequential
 from keras.layers import Dense, Activation
@@ -8,9 +9,12 @@ from keras.optimizers import SGD
 from tqdm import tqdm
 from keras import backend as K
 import tensorflow as tf
+import matplotlib.pyplot as plt
 tf.executing_eagerly()
-
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 weight = 1
+import time
 
 class sumtree:
     def __init__(self,capacity):
@@ -55,7 +59,7 @@ class sumtree:
         else:
             
             #print('right')
-            return self.retrive(right_leave, s)
+            return self.retrive(right_leave, s-self.tree[left_leave])
     def get(self, s):
         get_idx = self.retrive(0, s)
         #print(get_idx,self.data,self.tree)
@@ -75,10 +79,10 @@ class sumtree:
             random_right = pri_seg*(choose_n+1)
 
             random_s = np.random.uniform(random_left, random_right)
-            print(random_right,random_s,self.total())
-            #print(self.total(),choose_n, random_s,sumtree.tree,'random')
+            #print(random_right,random_s,self.total())
+            #####print(self.total(),choose_n, random_s,sumtree.tree,'random')
             choose_idx[choose_n], priority_choose, choose_batch[choose_n]=self.get(random_s)
-            priority_nominize = priority / self.total()
+            priority_nominize = priority_choose / self.total()
             ISweight[choose_n] = np.power(priority_nominize/min_priority, -beta)
             #print('sample',choose_batch)
         return choose_batch, choose_idx, ISweight, beta
@@ -88,7 +92,7 @@ def actionchoose(action_size,state):
     action_value = model.predict(state)
     return np.argmax(action_value)
 def mse_with_IS(y_true,y_pred):
-    print(y_true,'y_true')
+    #print(y_true,'y_true')
     weight_array=y_true.numpy()
     weight_use= weight_array[0][2]
     y_true_without_weight=tf.convert_to_tensor(np.reshape(weight_array[0][0:2],[1,2]))
@@ -105,7 +109,7 @@ if __name__ == "__main__":
     gamma = 0.95
     beta = 0.4
     beta_improve_perstep = 0.001
-    replay_inter=10
+    replay_inter=30
     sgd =   SGD(lr=0.01, decay=1e-6,momentum=0.9, nesterov=True)
     env = gym.make('CartPole-v1')
     action_size = env.action_space.n
@@ -113,45 +117,50 @@ if __name__ == "__main__":
     model = Sequential([
         Dense(24,input_dim=space_size),
         Activation('relu'),
-        Dense(36),
-        Activation('softmax'),
+        Dense(20),
+        Activation('sigmoid'),
         Dense(action_size),
         Activation('linear')
     ])
 
     
-    sumtree = sumtree(10)
+    sumtree = sumtree(128)
     model_=model
     model.compile(loss=mse_with_IS,
                     optimizer=sgd,
                     metrics = ['accuracy'],
                     run_eagerly=True)
-    for i in tqdm(range(1000)):
+    reward_plot=np.zeros(11)
+    reward_sum=0
+    for i in tqdm(range(10)):
+        reward_plot[i]=reward_sum
+        reward_sum=0
         state = env.reset()
         state = np.reshape(state, [1, space_size])
         for j in range(500):
-            env.render()
+            #env.render()
             action=actionchoose(action_size, state)
             #print(action)
             next_state, reward, done, _ = env.step(action)
-            reward = reward if not done else -10
+            reward = reward if not done else 0
             next_state = np.reshape(next_state,[1,space_size])
             action_target = np.argmax(model.predict(next_state))
             priority = np.abs(reward + gamma * model_.predict(next_state)[0][action_target] - model.predict(state)[0][action])
             sumtree.add(priority, (state, action, reward, next_state, done))             
             state = next_state
+            reward_sum=reward_sum+reward
             #print(sumtree.addnum,'addnum',sumtree.write)
             if done:
-                #print('end')
+                print('end')
                 break
-            if sumtree.addnum%replay_inter ==0 and sumtree.addnum/replay_inter>1:
+            #if sumtree.addnum%replay_inter ==0 and sumtree.addnum/replay_inter>5:
                 #print('do')
-
+            if sumtree.addnum> replay_inter:
                 minibatch,minibatch_idx,ISweight_use,beta =sumtree.sample(minibatch_size, beta, beta_improve_perstep)
                 model_=model
                 idx_num = 0
                 #print(sumtree.tree)
-                #print(minibatch_idx)
+                print(minibatch_idx)
                 #print(minibatch)
                 #print(sumtree.addnum,'addnum')
                 for state, action, reward, next_state, done in minibatch:
@@ -164,14 +173,24 @@ if __name__ == "__main__":
                     else:
                         action_target = np.argmax(model.predict(next_state))
                         traget_value_action = reward + gamma * model_.predict(next_state)[0][action_target]
+                        #print('here',action_target, model_.predict(next_state))
                     
                     target = model.predict(state)
+                    #print(target,'target',traget_value_action)
+
                     priority_change = abs(traget_value_action - target[0][action])
                     sumtree.update(priority_change,np.int(tree_idx))
-                    target[0][action]=weight*(traget_value_action-target[0][action])
-                    target[0][1-action]=0
+                    target[0][action]=traget_value_action
                     target_with_weight=np.append(target,weight_tree)
                     target_with_weight=np.reshape(target_with_weight,[1,3])
+                    #print(target_with_weight)
+                    starttime=time.time()
                     model.fit(state,target_with_weight,epochs=2, verbose=0)
+                    endtime=time.time()
+                    #print(endtime-starttime)
                     idx_num = idx_num + 1
+                    print('122344567',action_target,model_.predict(next_state))
                     #print(idx_num,'idx_num',minibatch)
+    plt.plot(reward_plot)
+    plt.show()
+    plt.show()
--- rlcode_python/sumtree.py
+++ rlcode_python/sumtree.py
@@ -37,9 +37,9 @@ class sumtree:
         self.write =self.write + 1
         self.update(p,idx)
         self.addnum =self.addnum + 1
-        if self.write > self.capacity:
+        if self.write >= self.capacity:
             self.write = 0 
-        print(self.tree)
+        #print(self.tree)
     def retrive(self,idx,s):
         
         #print(idx,'idx')
@@ -54,7 +54,7 @@ class sumtree:
         else:
             
             #print('right')
-            return self.retrive(right_leave, s)
+            return self.retrive(right_leave, s-self.tree[left_leave])
     def get(self, s):
         get_idx = self.retrive(0, s)
         #print(get_idx,self.data,self.tree)
@@ -62,11 +62,35 @@ class sumtree:
 
     def total(self):
         return self.tree[0]
+    def sample(self,n,beta,beta_improve_perstep):
+        choose_batch = np.zeros(n, dtype=object)
+        beta=np.min([1,beta+beta_improve_perstep]) 
+        ISweight = np.zeros(n)
+        choose_idx = np.zeros(n)
+        pri_seg = self.total()/n
+        #min_priority = np.min(self.tree[-self.capacity:])
+        min_priority =1
+        for choose_n in range(n):
+            random_left = pri_seg*choose_n
+            random_right = pri_seg*(choose_n+1)
+
+            random_s = np.random.uniform(random_left, random_right)
+            #print(random_right,random_s,self.total())
+            #print(self.total(),choose_n, random_s,sumtree.tree,'random')
+            choose_idx[choose_n], priority_choose, choose_batch[choose_n]=self.get(random_s)
+            priority_nominize = priority_choose / self.total()
+            ISweight[choose_n] = np.power(priority_nominize/min_priority, -beta)
+            #print('sample',choose_batch)
+        return choose_batch, choose_idx, ISweight, beta
 
 if __name__ == "__main__":
-	sumtree=sumtree(5)
-	sumtree.add(1,1)
-	sumtree.add(2,3)
-	sumtree.add(3,4)
-	q,k,j=sumtree.get(5.9)
-	print(q,k,j, sumtree.tree, sumtree.data)
+    sumtree=sumtree(32)
+    sumtree.add(1,1)
+    sumtree.add(2,3)   
+    sumtree.add(3,4)
+    for i in range(19):
+        sumtree.add(np.random.randint(1,4),5)
+       #print(sumtree.write)
+    q,k,j=sumtree.get(3.5)
+    minibatch,minibatch_idx,ISweight_use,beta =sumtree.sample(10, 1, 0)
+    print(minibatch_idx)
